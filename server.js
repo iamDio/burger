6d2692f1419f626f7808d9747bbb0c09f823a212
iamDio <claudio.villani00@gmail.com>
2017-09-29 01:05:19 -0400

    heroku test II

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.port || 3006;
 app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
